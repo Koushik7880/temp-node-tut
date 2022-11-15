649fb438ab52f42440b7bed5a2eee1fa176afa32
Koushik Das <koushik78bd@gmail.com>
2022-11-15 19:38:30 +0600

    t

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// 1.49
+const { readFile } = require('fs')
+// const { result } = require('lodash')
+
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+getText('./content/first.txt')
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
+
+const start = async () => {
+  const first = await getText('./content/first.txt')
+  console.log(first)
+}
+
+// start 3.10
